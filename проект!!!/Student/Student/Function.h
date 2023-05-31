e2bcecbc623b5f4a28653ded06b788e685481e96
TatianaYuzh <yuzhtanushka@yandex.ru>
2023-05-31 11:25:04 +0500

    project

---
+++ проект!!!/Student/Student/Function.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+
+using namespace  std;
+namespace table1{
+struct Id {
+	int number;
+};
+
+struct Initial {
+	string sutname,
+		patronymic,
+		name;
+};
+
+struct Date {
+	int day,
+		month,
+		year;
+};
+
+struct Group {
+	string number;
+};
+
+//структура данных
+struct Data {
+	Id _id;
+	Initial _initial;
+	Date _date;
+	Group _group;
+};
+
+//прототипы функций
+void DataEntry(Data* (&d), int& n); //ввод 
+void ReadingData(Data* (&d), int& n, string fileName); //чтение из файла
+void Print(Data* d, int n); //вывод
+void DataChange(Data* (&d), int n); //изменение
+void DeleteData(Data* (&d), int& n); //удаление
+void Copy(Data* (&d_n), Data* (&d_o), int n); //копия данных
+void Copy(Data& d_n, Data& d_o); //копия данных элемента
+void AddDate(Data* (&d), int& n); //добавить данные
+void DataSorting(Data* d, int n); //сортировка данных
+void SavingData(Data* d, int n, string fileName); //сохранение данных
+
+
+}
